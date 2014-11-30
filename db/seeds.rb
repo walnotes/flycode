a52eb390cc14a2ca6cd97fe6022aabd09ec5ec81
Walter <walterchkoning@yahoo.com>
2014-11-30 10:50:53 -0800

    Seeds file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# users = User.create([{name: 'KingKong', email: 'walterchkoning@yahoo.com', password: '123faker'}])
-User.new(name: "KingKong", email: "walterchkoning@yahoo.com", password: "123faker")
-# Organization.create(name:"Test", url: "http://www.test.com", linkedin: "https://www.linkedin.com/pub/walter-koning/1/b02/a51", user_id: User.first.user_id)
-Organization.new(name: "Test", url: "http://www.test.com", linkedin: "https://www.linkedin.com/pub/walter-koning/1/b02/a51", user_id: "1")
+# User.create(name: "KingKong", email: "walterchkoning@yahoo.com", password: "123faker")
+# Organization.create(name: "Test", url: "http://www.test.com", linkedin: "https://www.linkedin.com/pub/walter-koning/1/b02/a51", user_id: "1")
+User.create!(:name => "KingKong", :email => "walterchkoning@yahoo.com", :password => "123faker")
+Organization.create!(:name => "Test", :url => "http://www.test.com", :linkedin => "https://www.linkedin.com/pub/walter-koning/1/b02/a51", :user_id => "1")
 
 # User.new(name: "Example User", email: "user@example.com")
 # foo = User.create(name: "Foo", email: "foo@bar.com")
