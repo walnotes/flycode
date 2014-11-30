64c939e85058f742ea57c28594311cb82cef3a36
Walter <walterchkoning@yahoo.com>
2014-11-30 11:08:42 -0800

    Seeds file change

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 
 # This format worked. TODO: Delete this seed user after I figure out how to get the organization seed data working.
 #User.create!(:name => "KingKong", :email => "walterchkoning@yahoo.com", :password => "123faker", :password_confirmation => "123faker")
-Organization.create!(:name => "Test", :url => "http://www.test.com", :linkedin => "https://www.linkedin.com/pub/walter-koning/1/b02/a51", :user_id => 1)
+Organization.create!(:name => "Test", :url => "http://www.test.com", :linkedin => "https://www.linkedin.com/pub/walter-koning/1/b02/a51", :user => 1)
 
 # User.new(name: "Example User", email: "user@example.com")
 # foo = User.create(name: "Foo", email: "foo@bar.com")
