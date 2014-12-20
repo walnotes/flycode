7c916b705a135f2d96179ab74719258e5e0b25df
WKoning <walterchkoning@yahoo.com>
2014-12-20 15:35:38 -0800

    Attempting to delete a profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  has_many :organizations
+  has_many :organizations, dependent: :destroy
   accepts_nested_attributes_for :organizations
 
   has_many :microposts, dependent: :destroy
